4d78a7e77867ca6db1ae32dff3f27c0bdd5b767e
Yogi Bagus <58897819+yogibagus@users.noreply.github.com>
2022-06-26 23:37:52 +0700

    fix bug

--- js/map-config.js
+++ js/map-config.js
@@ -12,57 +12,59 @@ function getLocation() {
 getLocation();
 
 // show the position and run the mapbox
+var latitude;
+var longitude;
 function showPosition(position) {
-	var latitude = position.coords.latitude;
-	var longitude = position.coords.longitude;
-
-	// set destination
-	var url_string = window.location.href; //window.location.href
-	var url = new URL(url_string);
-	var longitudeDestination = url.searchParams.get("longitude");
-	var latitudeDestination = url.searchParams.get("latitude");
-	if(longitudeDestination && latitudeDestination){
-		
-	}else{
-		longitudeDestination = null;
-		latitudeDestination = null;
-	}
-	
-
-	mapboxgl.accessToken = 'pk.eyJ1IjoieW9nc3RlciIsImEiOiJja2o3ank4d3I2aW5rMnFsYm5wZmE2OWw1In0.F179z2jXPmVBSRYxtrzzzA';
-	var map = new mapboxgl.Map({
-		container: 'map',
-		style: 'mapbox://styles/mapbox/streets-v11',
-		center: [112.16809,-8.09774],
-		zoom: 17,	
-	});
-
-	var directions = new MapboxDirections({
-		accessToken: mapboxgl.accessToken,
-		unit: 'metric',
-		profile: 'mapbox/driving'
-	});
-
-	map.addControl(directions,'top-left');
-
-	map.on('load',  function() {
-		directions.setOrigin([longitude,latitude]); 
-		
-		if(latitudeDestination != null && longitudeDestination != null || latitudeDestination != undefined && longitudeDestination != undefined){
-			console.log(longitudeDestination +","+ latitudeDestination);
-			directions.setDestination([longitudeDestination, latitudeDestination]); // can be address
-		}
-	})
-
-	// Add geolocate control to the map.
-	map.addControl(
-		new mapboxgl.GeolocateControl({
-			positionOptions: {
-				enableHighAccuracy: true
-			},
-			trackUserLocation: true
-		})
-	);
+	latitude = position.coords.latitude;
+	longitude = position.coords.longitude;
 }
+
+// set destination
+var url_string = window.location.href; //window.location.href
+var url = new URL(url_string);
+var longitudeDestination = url.searchParams.get("longitude");
+var latitudeDestination = url.searchParams.get("latitude");
+if(longitudeDestination && latitudeDestination){
+	
+}else{
+	longitudeDestination = null;
+	latitudeDestination = null;
+}
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoieW9nc3RlciIsImEiOiJja2o3ank4d3I2aW5rMnFsYm5wZmE2OWw1In0.F179z2jXPmVBSRYxtrzzzA';
+var map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/mapbox/streets-v11',
+	center: [112.16809,-8.09774],
+	zoom: 17,	
+});
+
+var directions = new MapboxDirections({
+	accessToken: mapboxgl.accessToken,
+	unit: 'metric',
+	profile: 'mapbox/driving'
+});
+
+map.addControl(directions,'top-left');
+
+map.on('load',  function() {
+	directions.setOrigin([longitude,latitude]); 
+	
+	if(latitudeDestination != null && longitudeDestination != null || latitudeDestination != undefined && longitudeDestination != undefined){
+		console.log(longitudeDestination +","+ latitudeDestination);
+		directions.setDestination([longitudeDestination, latitudeDestination]); // can be address
+	}
+})
+
+// Add geolocate control to the map.
+map.addControl(
+	new mapboxgl.GeolocateControl({
+		positionOptions: {
+			enableHighAccuracy: true
+		},
+		trackUserLocation: true
+	})
+);
 
 
